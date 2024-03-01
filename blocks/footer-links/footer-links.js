9a5ef6d2c76634408bed693c41a7946b99c89305
nicholasc-ensemble <nicholasc@ensemble.com>
2024-03-01 13:42:56 -0800

    ADOFRN-31: Create blocks for footer and styling

---
+++ blocks/footer-links/footer-links.js
@@ -0,0 +1,39 @@
+/**
+ * loads and decorates the footer
+ * @param {Element} block The footer block element
+ */
+export default async function decorate(block) {
+  block.children[0].classList.add('footer-links-content');
+
+  const sections = block.children[0].getElementsByTagName('div');
+  [...sections].forEach((elem) => {
+    const header = elem.getElementsByTagName('h1');
+    const linkContainer = document.createElement('div');
+
+    const links = elem.querySelectorAll('li a');
+    [...links].forEach((link) => {
+      if (link.href.includes('.pdf')) {
+        link.classList.add('link-download-icon');
+      }
+    });
+
+    const buttons = elem.querySelectorAll('a[class*="button"]');
+    buttons.forEach((button) => {
+      const buttonContainer = document.createElement('div');
+      const buttonIconContainer = document.createElement('span');
+      const buttonIcon = document.createElement('i');
+
+      button.className = 'icon-button';
+      buttonContainer.innerText = button.innerText;
+      button.innerText = '';
+
+      buttonIconContainer.append(buttonIcon);
+      buttonContainer.append(buttonIconContainer);
+      button.append(buttonContainer);
+    });
+
+    linkContainer.append(...links);
+    linkContainer.className = 'footer-links-container';
+    elem.replaceChildren(...header, linkContainer, ...buttons);
+  });
+}
---
+++ blocks/footer-socials/footer-socials.js
@@ -0,0 +1,25 @@
+/**
+ * loads and decorates the footer
+ * @param {Element} block The footer block element
+ */
+export default async function decorate(block) {
+  const icons = block.getElementsByTagName('a');
+  const logo = block.getElementsByTagName('picture');
+  const subtext = [...block.getElementsByTagName('p')]
+    .filter((elem) => elem.textContent.length > 0 && elem.children.length < 1);
+
+  const iconsContainer = document.createElement('div');
+  iconsContainer.append(...icons);
+  iconsContainer.className = 'footer-socials-icons-container';
+
+  const logoContainer = document.createElement('div');
+  logoContainer.append(...logo);
+  logoContainer.className = 'footer-socials-logo-container';
+
+  const textContainer = document.createElement('div');
+  textContainer.append(...subtext);
+  textContainer.className = 'footer-socials-text-container';
+
+  block.children[0].classList.add('footer-socials-content');
+  block.children[0].replaceChildren(logoContainer, textContainer, iconsContainer);
+}
